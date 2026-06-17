ff91774dd6d3e23092484174f387758e2a24ec91
Ethan Marantz <ebmarantz@gmail.com>
2026-06-17 02:21:50 -0400

    refactored MetricSnapshotImpl so that constructor is private and friended InstanceUtil
    Signed-off-by: Ethan Marantz <ebmarantz@gmail.com>

--- source/server/server.h
+++ source/server/server.h
@@ -469,11 +469,6 @@ private:
 //                     copying and probably be a cleaner API in general.
 class MetricSnapshotImpl : public Stats::MetricSnapshot {
 public:
-  // MetricSnapshotImpl captures a snapshot of metrics by latching the delta usage, and optionally
-  // marking the stats as used.
-  explicit MetricSnapshotImpl(Stats::Store& store, Upstream::ClusterManager& cluster_manager,
-                              TimeSource& time_source);
-
   // Stats::MetricSnapshot
   const std::vector<CounterSnapshot>& counters() override { return counters_; }
   const std::vector<std::reference_wrapper<const Stats::Gauge>>& gauges() override {
@@ -492,6 +487,12 @@ public:
   SystemTime snapshotTime() const override { return snapshot_time_; }
 
 private:
+  friend class InstanceUtil;
+  // MetricSnapshotImpl captures a snapshot of metrics by latching the delta usage, and optionally
+  // marking the stats as used.
+  explicit MetricSnapshotImpl(Stats::Store& store, Upstream::ClusterManager& cluster_manager,
+                              TimeSource& time_source);
+
   std::vector<Stats::CounterSharedPtr> snapped_counters_;
   std::vector<CounterSnapshot> counters_;
   std::vector<Stats::GaugeSharedPtr> snapped_gauges_;
